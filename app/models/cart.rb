d7cc2018e68158e2d8019af6ad2dd8a3e4ac9589
Dmitry <pchellovod@gmail.com>
2018-07-17 15:39:13 +0500

    add add_item and remove_position to cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -7,4 +7,16 @@ class Cart < ApplicationRecord
   has_many :items, through: :positions
 
   has_and_belongs_to_many :items
+
+  def add_item(i)
+    if items.include?(i)
+      positions.where(item_id: i.id).first.increment!(:quantity)
+    else
+      positions.create(item: i, quantity: 1)
+    end
+  end
+
+  def remove_position(position_to_remove_id)
+    positions.where(item_id: position_to_remove_id).first.try(:destroy)
+  end
 end
