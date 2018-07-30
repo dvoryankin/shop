5e23369d03d185b00320808ef4452f2c00e93d99
Dmitry <pchellovod@gmail.com>
2018-07-30 12:01:07 +0500

    test view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,7 @@ class ItemsController < ApplicationController
     @variable = "#{params[:first_name]}"
 
     @items_count = Item.count
-    render html: "It is ItemsController #{params[:first_name]} #{params[:family_name]} ! 
-    \t #{@variable } #{@items_count}"
+    # render html: "It is ItemsController #{params[:first_name]} #{params[:family_name]} ! #{@variable } #{@items_count} #{params}"
 
   end
 end
